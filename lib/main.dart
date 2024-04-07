b068c988a624095cf762dab3c59bb434ed39717d
Datvan10 <20050068@student.bdu.edu.vn>
2024-04-07 22:57:38 +0700

    Add router.dart file and improve code

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,10 @@ import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:highlandcoffeeapp/components/pages/not_found_404_page.dart';
-import 'package:highlandcoffeeapp/components/pages/payment_result_page.dart';
-import 'package:highlandcoffeeapp/components/pages/result_search_product_with_not_found_404_page.dart';
-import 'package:highlandcoffeeapp/pages/login_and_register/admin/login_admin_with_email_and_password_page.dart';
-import 'package:highlandcoffeeapp/pages/login_and_register/user/login_user_with_email_and_password_page.dart';
-import 'package:highlandcoffeeapp/widgets/choose_login_type_page%20.dart';
-import 'package:highlandcoffeeapp/components/pages/order_page.dart';
-import 'package:highlandcoffeeapp/components/pages/favorite_product_page.dart';
-import 'package:highlandcoffeeapp/components/pages/list_product_page.dart';
-import 'package:highlandcoffeeapp/components/pages/product_popular_page.dart';
+import 'package:highlandcoffeeapp/routes/route.dart';
 import 'package:highlandcoffeeapp/firebase_options.dart';
-import 'package:highlandcoffeeapp/pages/admin/admin_page.dart';
-import 'package:highlandcoffeeapp/pages/auth/auth_user_page.dart';
-import 'package:highlandcoffeeapp/pages/cart/cart_page.dart';
-import 'package:highlandcoffeeapp/pages/home/home_page.dart';
-import 'package:highlandcoffeeapp/pages/introduce/introduce_page1.dart';
-import 'package:highlandcoffeeapp/pages/introduce/introduce_page2.dart';
-import 'package:highlandcoffeeapp/pages/user/page/my_order_page.dart';
-import 'package:highlandcoffeeapp/pages/user/page/update_user_profille.dart';
 import 'package:highlandcoffeeapp/pages/welcome/welcome_page.dart';
 import 'package:highlandcoffeeapp/themes/theme.dart';
-import 'package:highlandcoffeeapp/utils/bill/payment/payment_qr_code.dart';
 
 
 
@@ -68,23 +50,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
 
       home: WelcomePage(),
-      getPages: [
-        GetPage(name: '/welcome_page', page: () => const WelcomePage()),
-        GetPage(name: '/introduce_page1', page: () => const IntroducePage1()),
-        GetPage(name: '/introduce_page2', page: () => const IntroducePage2()),
-        GetPage(name: '/choose_login_type_page', page: () => ChooseLoginTypePage()),
-        GetPage(name: '/auth_page', page: () => const AuthUserPage()),
-        GetPage(name: '/home_page', page: () => const HomePage()),
-        GetPage(name: '/list_product_page', page:() => const ListProductPage()),
-        GetPage(name: '/product_popular_page', page:() => const ProductPopularPage()),
-        GetPage(name: '/favorite_product_page', page:() => const FavoriteProductPage()),
-        GetPage(name: '/cart_page', page:() => const CartPage()),
-        GetPage(name: '/bill_page', page:() => const BillPage()),
-        GetPage(name: '/admin_page', page:() => const AdminPage()),
-        GetPage(name: '/update_user_profile_page', page:() => const UpdateUserProfilePage()),
-        GetPage(name: '/my_order_page', page:() => const MyOrderPage()),
-        GetPage(name: '/payment_result_page', page:() => const PaymentResultPage()),
-      ],
+      getPages: getPages,
       theme: ThemeData(
         primaryColor: primaryColors,
         primarySwatch: Colors.red,
